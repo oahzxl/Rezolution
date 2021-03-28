8cf727074b5070d0ce5f6004e8c566accbb727be
zxl <43881818+oahzxl@users.noreply.github.com>
2021-03-28 11:13:25 +0800

    Initial commit

---
+++ tools/run.sh
@@ -0,0 +1,4 @@
+bash tools/dist_train.sh configs/_mine/ccnet_r50-d8_512x512_20k_voc121.py 2
+bash tools/dist_train.sh configs/_mine/ccnet_r50-d8_512x512_20k_voc122.py 2
+bash tools/dist_train.sh configs/_mine/ccnet_r50-d8_512x512_20k_voc123.py 2
+bash tools/dist_train.sh configs/_mine/ccnet_r50-d8_512x512_20k_voc124.py 2
