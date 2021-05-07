9a63cf1adc78d8ac83ba49b2998a906eccd0bbd1
zxl <43881818+oahzxl@users.noreply.github.com>
2021-05-07 13:16:58 +0800

    2rev 7631

--- tools/run3.sh
+++
@@ -1,2 +0,0 @@
-CUDA_VISIBLE_DEVICES=2,1 PORT=29003 bash tools/dist_train.sh configs/_my/revnet_psp_40k_up_pos_voc12aug.py 2
-CUDA_VISIBLE_DEVICES=2,1 PORT=29003 bash tools/dist_train.sh configs/_my/revnet_psp_40k_up_pos_voc12aug.py 2
--- tools/run5.sh
+++ tools/run5.sh
@@ -1,2 +1,5 @@
-CUDA_VISIBLE_DEVICES=2,1 PORT=29003 bash tools/dist_train.sh configs/_my/revnet_psp_40k_up_2rev_voc12aug.py 2
-CUDA_VISIBLE_DEVICES=2,1 PORT=29003 bash tools/dist_train.sh configs/_my/revnet_psp_40k_up_2rev_voc12aug.py 2
+CUDA_VISIBLE_DEVICES=2,1 PORT=29003 bash tools/dist_train.sh configs/_my/revnet_psp_40k_up_2rev2stride_voc12aug.py 2
+CUDA_VISIBLE_DEVICES=2,1 PORT=29003 bash tools/dist_train.sh configs/_my/revnet_psp_40k_up_2rev2stride_voc12aug.py 2
+CUDA_VISIBLE_DEVICES=2,1 PORT=29003 bash tools/dist_train.sh configs/_my/revnet_psp_40k_up_2rev2stride_voc12aug.py 2
+CUDA_VISIBLE_DEVICES=2,1 PORT=29003 bash tools/dist_train.sh configs/_my/revnet_psp_40k_up_2rev2stride_voc12aug.py 2
+CUDA_VISIBLE_DEVICES=2,1 PORT=29003 bash tools/dist_train.sh configs/_my/revnet_psp_40k_up_2rev2stride_voc12aug.py 2
